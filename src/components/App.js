a7b70a7abdeffd90a9aff0c1ab654e25b0728e79
Carolyne munyiva <munyivacarolyne571@gmail.com>
2024-05-06 23:04:41 +0300

    complete effect fetch lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React,{useState, useEffect} from "react";
+
+function App(){
+const[dogs, setDogs]=useState("")
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response)=> response.json())
+        .then((data)=>{setDogs(data.message)
+        })
+    },[]);
+    if (!dogs) return<p>Loading...</p>
+    return (
+       <div>
+       <img src={dogs} alt="A Random Dog "/>
+       
+        </div> 
+    )
+ }
+
+export default App;
